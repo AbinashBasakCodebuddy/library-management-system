60ec4cd027dd3646d9faa647d31664cf42053278
AbinashBasakCodebuddy <abinashbasak@codebuddy.co>
2026-04-21 17:57:30 +0530

    update global error handle, added prisma error handler

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -7,6 +7,7 @@ import {
     Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { PrismaClientKnownRequestError } from 'src/generated/prisma/internal/prismaNamespace';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -14,20 +15,39 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     constructor() {}
 
+    private getExceptionMessage(exception: unknown) {
+        if (exception instanceof HttpException) {
+            const response = exception.getResponse();
+            return response;
+        }
+
+        if (exception instanceof PrismaClientKnownRequestError) {
+            const response = exception.message;
+            return response;
+        }
+
+        return 'Internal server error';
+    }
+
+    private getExceptionCode(exception: unknown) {
+        if (exception instanceof HttpException) {
+            return exception.getStatus();
+        }
+
+        if (exception instanceof PrismaClientKnownRequestError) {
+            return HttpStatus.BAD_REQUEST;
+        }
+
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
     catch(exception: unknown, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
 
-        const httpStatus =
-            exception instanceof HttpException
-                ? exception.getStatus()
-                : HttpStatus.INTERNAL_SERVER_ERROR;
-
-        const message =
-            exception instanceof HttpException
-                ? exception.getResponse()
-                : 'Internal server error';
+        const httpStatus = this.getExceptionCode(exception);
+        const message = this.getExceptionMessage(exception);
 
         const path = request.url;
         const method = request.method;
